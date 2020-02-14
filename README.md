7814b87fd115edde1b781720add0ff74afe19bf8
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2020-02-14 20:58:37 +0900

    [add] initial commit./

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kube-go-template
+
+This is a template project for angular application works on kubernetes cluster. This template provides minimum k8s manifest and docker file.
+
+# Usage
+
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ispec-inc/kube-go-template/master/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+git clone https://github.com/ispec-inc/kube-go-template.git
+read -p "application name?: " app_name
+read -p "use dev? (y/N): " yn
+case "$yn" in
+    [yY]*) use_dev=true ;;
+    *) use_dev=false ;;
+esac
+read -p "use stg? (y/N): " yn
+case "$yn" in
+    [yY]*) use_dev=true ;;
+    *) use_dev=false ;;
+esac
+
+declare -a envs=("prod")
+if $use_dev;then
+envs+=("dev")
+fi
+
+if $use_stg;then
+envs+=("stg")
+fi
+
+cp kube-go-template/container/Dockerfile Dockerfile
+cp kube-go-template/container/docker-compose.yml docker-compose.yml
+cp -r kube-go-template/nginx nginx
+sed -i "" -e "s/{{app_name}}/$app_name/" docker-compose.yml
+mkdir k8s
+for env in ${envs[@]}
+do
+mkdir k8s/${env}
+cp kube-go-template/manifest/* k8s/"${env}"
+find k8s/"${env}"/*.yml | xargs sed -i "" -e "s/{{app_name}}/$env-$app_name/"
+done
+rm -rf kube-go-template

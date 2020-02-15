ae06703b3a3a398c00d0e11ba0642482fe4c61eb
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2020-02-15 15:42:11 +0900

    [add] codes.

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ case "$yn" in
     [yY]*) use_dev=true ;;
     *) use_dev=false ;;
 esac
+
 read -p "use stg? (y/N): " yn
 case "$yn" in
     [yY]*) use_dev=true ;;
@@ -13,6 +14,7 @@ case "$yn" in
 esac
 
 declare -a envs=("prod")
+
 if $use_dev;then
 envs+=("dev")
 fi
@@ -23,6 +25,12 @@ fi
 
 cp kube-go-template/container/Dockerfile Dockerfile
 cp kube-go-template/container/docker-compose.yml docker-compose.yml
+
+if [ ! -d  ".github/workflows"];then
+  mkdir -p .github/workflows
+fi
+cp workflow.yml .github/workflows/prod.yml
+
 sed -i "" -e "s/{{app_name}}/$app_name/" docker-compose.yml
 mkdir k8s
 for env in ${envs[@]}
@@ -30,5 +38,6 @@ do
 mkdir k8s/${env}
 cp kube-go-template/manifest/* k8s/"${env}"
 find k8s/"${env}"/*.yml | xargs sed -i "" -e "s/{{app_name}}/$app_name/"
+find workflow.yml | xargs sed -i "" -e "s/{{app_name}}/$app_name/"
 done
 rm -rf kube-go-template
